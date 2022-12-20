73d016addd0349e50dc712fa6d3cef002fb6fabb
Thandar Win <thadarwin.1551992@outlook.com>
2022-12-20 16:45:50 +0630

    WIP sign up.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -9,7 +11,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit {
 
   form!: FormGroup;
-  constructor(private fb:FormBuilder){
+  constructor(private fb:FormBuilder,private auth:AuthService,private router:Router){
 
   }
   ngOnInit(): void {
@@ -19,7 +21,30 @@ export class LoginComponent implements OnInit {
     });
     
   }
-  login(){
+  async login(){
+    console.log(this.form.value);
+    const res= await this.auth.login(this.form.value);
+    console.log('login ',res);
+    if(res.error){
+      //To  do show error alert
+      console.log('login account error')
 
+
+    }else{
+      this.router.navigateByUrl('/app',{replaceUrl:true})
+    }
+  }
+  async createAccount(){
+    console.log(this.form.value);
+    const res = await this.auth.createAccount(this.form.value);
+    console.log('Create Account',res);
+    if(res.error){
+      //To  do show error alert
+      console.log('create account error')
+
+
+    }else{
+      this.router.navigateByUrl('/app',{replaceUrl:true})
+    }
   }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production :false,
+    supabaseUrl:'https://zvtmhytmeguysrwppedq.supabase.co',
+    supabaseKey:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp2dG1oeXRtZWd1eXNyd3BwZWRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzE1MjI0OTQsImV4cCI6MTk4NzA5ODQ5NH0._QZPvz_TX1_Q3lEHFH8E16nI2sbf6AoB4F-jwMzjXB8'
+}
